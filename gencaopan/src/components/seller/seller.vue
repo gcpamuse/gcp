3ec7e5fc533fb89e8e7541ff7dfe0be5526c9b4c
nai <eamil@qq.com>
2019-11-05 16:48:29 +0800

    a

--- gencaopan/src/components/seller/seller.vue
+++ gencaopan/src/components/seller/seller.vue
@@ -13,7 +13,7 @@
 				<van-cell icon="user-dingdan" title="使用说明" :to="{ name: 'applyShow'}" isLink />	
 				<van-cell icon="user-ye" title="我的关注" :to="{ name: 'myconcem'}" isLink />	
 				<van-cell icon="user-jifen" title="邀请有奖" :to="{ name: 'prizes'}" isLink />
-				<van-cell icon="user-dingdan" title="我的订阅" isLink />	
+				<van-cell icon="user-dingdan" title="我的订阅" :to="{ name: 'teacherSubscribe'}" isLink />	
 				<van-cell icon="user-ye" title="推送设置" isLink />	
 				<van-cell icon="user-jifen" title="商务合作" isLink />
 			</van-cell-group>
