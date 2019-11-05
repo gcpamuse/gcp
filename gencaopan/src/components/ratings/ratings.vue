86d6c25cac8a6f82f8addde0acc652ffd6bdfc34
yangfan <872404816@qq.com>
2019-11-05 14:11:38 +0800

    1

--- gencaopan/src/components/ratings/ratings.vue
+++ gencaopan/src/components/ratings/ratings.vue
@@ -9,7 +9,7 @@
 		<div class="zimeiti">自媒体</div>
 		<div style="height:6px;background:#f2f2f2;"></div>
 		<div class="index-tab"> 
-			<div class="tabs" v-for="(image, index) in 6" v-bind="index" :key="index">
+			<div class="tabs" v-for="(image, index) in 6" v-bind="index" :key="image">
 				<div class="media-content">  
 					<div class="media-panel"> 
 						<div class="panel-left"> 
