861693b275a3d8e7e648effdda52c2f051261160
nai <eamil@qq.com>
2019-11-05 19:02:06 +0800

    s

---
+++ gencaopan/src/components/applyManager.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="push-set"> 
+    <h2>使用说明</h2>
+      
+</div> 
+	
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="less" scoped="scoped">
+
+</style>
