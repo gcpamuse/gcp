24a8d1ae4d149c0ac349d0eaf57950208b7ae615
nai <eamil@qq.com>
2019-11-05 18:34:03 +0800

    a

--- gencaopan/src/components/seller/seller.vue
+++ gencaopan/src/components/seller/seller.vue
@@ -19,7 +19,7 @@
 			</van-cell-group>
 		</div>
 		<div class="btuton">
-			<van-button size="large" class="btu" :click="scanCode">加盟代理</van-button>
+			<van-button size="large" class="btu" @click="scanCode">加盟代理</van-button>
 		</div>	
 		<div style="height:50px;"></div>
 	</div>
