c0fb08f44e1902ea59dc456a7fa36bd9a6f74152
yangfan <872404816@qq.com>
2019-11-07 17:40:41 +0800

    1

--- gencaopan/src/components/applyInvestor.vue
+++ gencaopan/src/components/applyInvestor.vue
@@ -14,9 +14,9 @@
             />
         </van-cell-group>
         <van-radio-group v-model="radio">
-            <van-radio name="1" icon-size="12px" checked-color="#e84d48">
+            <van-radio name="1" icon-size="14px" checked-color="#e84d48">
                 <span>我已知晓相关风险，确认申请。</span> 
-                <van-icon slot="icon" name="danxuan" />
+                <van-icon slot="icon" size="10px" name="danxuan" />
             </van-radio>
         </van-radio-group> 
         <div class="but">
