1aa5eeeed1ad4c47ad761bcc220364495338c0c8
yangfan <872404816@qq.com>
2019-11-08 18:03:33 +0800

    1

--- gencaopan/src/components/subscribes.vue
+++ gencaopan/src/components/subscribes.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
-      1111111111111111111111111111111
+        <van-dialog
+        v-model="show"
+        >
+            <p class="guanzhu">请先关注</p>
+            <p class="p_concent">请您关注本微信号，进而查看本软件信息和各项功能。长按二维码关注本微信号(跟操盘)</p>
+            <p style="text-align:center;">
+                <img src="../img/ewm.jpg" alt="跟操盘" style="width:60%;" />
+            </p>
+        </van-dialog>
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            show: true
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped="scoped">
+.guanzhu{
+    text-align: center;
+    margin-top: 15px;
+    font-size: 15px;
+    color: #333;
+}
+.p_concent{
+    padding: 0 22px;
+    margin-top: 15px;
+    font-size: 14px;
+    color: #888;
+}
+.van-dialog {
+    position: fixed;
+    top: 62%;
+    left: 50%;
+    border-radius: 10px;
+}
+.van-dialog__header {
+    padding-top: 10px;
+}
 </style>
