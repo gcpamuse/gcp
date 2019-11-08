c2bd5d4c673af588146a45c2fbcf10ad6a69e457
nai <eamil@qq.com>
2019-11-08 14:39:42 +0800

    f

--- gencaopan/src/components/futuresDetails.vue
+++ gencaopan/src/components/futuresDetails.vue
@@ -17,54 +17,57 @@
                     <a class="book-btn charge" href="#curve" data-rel="popup" data-position-to="window" data-transition="fade">+关注</a>
                 </div> 
             </div> 
-            <div class="toast-mask" v-show="toast_control" @click="toast_control = false"></div>
-            <ul class="superior-nav fix"> 
-                <li class="active" @click="toJiaoYi" style="width:24.5%;border-right: 1px solid #eee;">交易概况</li> 
-                <li @click="toJIngZhi" style="width:24.5%;border-right: 1px solid #eee;">净值分析</li> 
-                <li @click="toChiCang" style="width:24.5%;border-right: 1px solid #eee;">实时持仓</li> 
-                <li @click="toChengJao" style="width:24.5%">当日成交</li> 
-            </ul>
-            <div class="superior-content"> 
-                <div class="superior-con" v-if="xianshi">
-                    <ary></ary>
-                    
-                    <p class="tip">订阅该导师后，即可查看该导师的交易记录</p> 
-                    <h5 class="numone">历史交易记录</h5>    
-                    <div class="tradingRecord-state"> 
-                        <a class="dingyue">订 阅</a>
-                        <span class="fl">后可查看全部交易记录</span>
-                    </div>
-                
-                </div> 
-                <div class="superior-con" v-if="xshi">
-                    <netWorth></netWorth>
-                    <h5 class="numone">出入金</h5> 
-                    <div class="jy_crj"> 
-                        <div>出入金</div> 
-                        <div>金额</div> 
-                        <div>日期<font color="red">(*半年内)</font></div> 
-                    </div> 
-                    <div class="jy_count"> 
-                        <div>入金</div> 
-                        <div><font color="red">+30000</font></div> 
-                        <div>2019-10-30</div> 
-                    </div> 
-                </div>
-                <div class="tradingRecord-state" v-if="sshi"> 
-                    <a class="dingyue">订 阅</a>
-                    <span class="fl">后可查看全部交易记录</span>
-                </div>
-                <div class="tradingRecord-state" v-if="dshi"> 
-                    <a class="dingyue">订 阅</a>
-                    <span class="fl">后可查看全部交易记录</span>
-                </div> 
+            <div class="superior-content">
+                <van-tabs active="b" line-height='0'>
+                    <van-tab title="交易概况" name="a">
+                        <div class="superior-con">
+                            <ary></ary>
+                            
+                            <p class="tip">订阅该导师后，即可查看该导师的交易记录</p> 
+                            <h5 class="numone">历史交易记录</h5>    
+                            <div class="tradingRecord-state"> 
+                                <a class="dingyue">订 阅</a>
+                                <span class="fl">后可查看全部交易记录</span>
+                            </div>
+                        
+                        </div> 
+                    </van-tab>
+                    <van-tab title="净值分析" name="b">
+                        <div class="superior-con">
+                            <netWorth></netWorth>
+                            <h5 class="numone">出入金</h5> 
+                            <div class="jy_crj"> 
+                                <div>出入金</div> 
+                                <div>金额</div> 
+                                <div>日期<font color="red">(*半年内)</font></div> 
+                            </div> 
+                            <div class="jy_count"> 
+                                <div>入金</div> 
+                                <div><font color="red">+30000</font></div> 
+                                <div>2019-10-30</div> 
+                            </div> 
+                        </div>
+                    </van-tab>
+                    <van-tab title="实时持仓" name="c">
+                        <div class="tradingRecord-state"> 
+                            <a class="dingyue">订 阅</a>
+                            <span class="fl">后可查看全部交易记录</span>
+                        </div>
+                    </van-tab>
+                    <van-tab title="当日成交" name="d">
+                        <div class="tradingRecord-state"> 
+                            <a class="dingyue">订 阅</a>
+                            <span class="fl">后可查看全部交易记录</span>
+                        </div>
+                    </van-tab>
+                </van-tabs>
             </div>
             <div class="tabbar"> 
                 <div class="tab">
                     <div class="caopan">操盘学习</div>
                     <div class="fuzhi" @click="toRenZheng">立即复制</div>
                 </div> 
-            </div>  
+            </div>
         </div> 
     </div>
     
@@ -82,49 +85,11 @@ export default {
                 backgroundRepeat: "no-repeat",
                 backgroundSize:'100% 100%'
             },
-            tabsItem: [
-                {
-                name: "交易概况",
-                status: 0
-                },
-                {
-                name: "我的净值",
-                status: 1
-                }
-            ],
-            xianshi: true,
-            xshi: false,
-            sshi: false,
-            dshi: false,
-            active:0, 
-            toast_control: false,
+            
         }
     },
     methods:{
-        toJiaoYi(){
-            this.xianshi=true;
-            this.xshi=false;
-            this.sshi=false;
-            this.dshi=false;
-        },
-        toJIngZhi(){
-            this.xianshi=false;
-            this.xshi=true;
-            this.sshi=false;
-            this.dshi=false;
-        },
-        toChiCang(){
-            this.xianshi=false;
-            this.xshi=false;
-            this.sshi=true;
-            this.dshi=false;
-        },
-        toChengJao(){
-            this.xianshi=false;
-            this.xshi=false;
-            this.sshi=false;
-            this.dshi=true;
-        },
+        
         toRenZheng(){
             this.$router.push({name: 'applyInvestor'})
         }
@@ -182,38 +147,34 @@ export default {
             } 
         }
     }
-    .superior-nav{
-        display: flex;
-        text-align: center;
-        font-size: 14px;
-        li{
-            padding: 15px 0;
-            background-color: #f6f6f6;
-            color: #666!important;
-        }
-    }
+    .van-tabs{
+		/deep/ .van-tab--active{
+			color: red;
+		}
+	}
     .superior-content{
-       .superior-con{
-           .numone{
-               padding: 10px;
-               color: #3333;
-               border-bottom: 2px solid #eee;
-               font-size: 15px;
-           }
-           .table-m1{
-               width: 100%;
-               text-align: center;
-               font-size: 12px;
+        
+        .superior-con{
+            .numone{
+                padding: 10px;
+                color: #3333;
+                border-bottom: 2px solid #eee;
+                font-size: 15px;
+            }
+            .table-m1{
+                width: 100%;
+                text-align: center;
+                font-size: 12px;
             //    border-bottom: 2px solid #eee;
-               color: #666;
-               tr{
-                   line-height: 25px;
-               }
-               .xuxian{
-                   border-bottom: 1px dashed #eee;
-               }
-           }
-           .tip{
+                color: #666;
+                tr{
+                    line-height: 25px;
+                }
+                .xuxian{
+                    border-bottom: 1px dashed #eee;
+                }
+            }
+            .tip{
                 position: relative;
                 margin: .750rem auto;
                 width: 90%;
@@ -225,12 +186,12 @@ export default {
                 font-size: .875rem;
                 line-height: 2.2;
                 border-radius: 5px;
-           }
-           .tradingRecord-state{
-               text-align: center;
-               padding-top: 20px;
-               margin-bottom: 50px;
-               .dingyue{
+            }
+            .tradingRecord-state{
+                text-align: center;
+                padding-top: 20px;
+                margin-bottom: 50px;
+                .dingyue{
                     display: inline-block;
                     border-radius: .5rem;
                     width: 6rem;
@@ -240,28 +201,30 @@ export default {
                     text-align: center;
                     background-color: #f24848;
                     color: #fff; 
-               }
-               .fl{
-                   font-size: 12px;
-                   color: #333;
-               }
-           }
-           .jy_crj,.jy_count{
-               display: flex;
-               font-size: 13px;
-               text-align: center;
-               border-bottom: 1px dashed #eee;
-               div{
-                   width: 33.33%;
-                   padding: 10px 0;
-               }
-           }
-           .jy_crj{
+                }
+                .fl{
+                    font-size: 12px;
+                    color: #333;
+                }
+            }
+            .jy_crj,.jy_count{
+                display: flex;
+                font-size: 13px;
+                text-align: center;
+                border-bottom: 1px dashed #eee;
+                div{
+                    width: 33.33%;
+                    padding: 10px 0;
+                }
+            }
+            .jy_crj{
                 font-weight: bold;
                 border-bottom: 1px solid #eee;
-           }
-       }
-        .tradingRecord-state{
+            }
+        }
+    }
+    
+    .tradingRecord-state{
             text-align: center;
             padding-top: 20px;
             margin-bottom: 50px;
@@ -281,7 +244,7 @@ export default {
                 color: #333;
             }
         }
-    }
+    
     .toast-mask {
         position: fixed;
         top: 0;
