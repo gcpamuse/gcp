6847ee4b63b7a2a08ab8f9c052a2913b080022f3
yangfan <872404816@qq.com>
2019-11-09 12:20:21 +0800

    1

--- gencaopan/src/components/release.vue
+++ gencaopan/src/components/release.vue
@@ -8,7 +8,7 @@
         </van-cell-group>
         <div class="item-content"> 
             <textarea class='content' placeholder="填写交易策略，行情分析，最少100字，建议写上买入区间，止损参考。禁止广告，恶意刷屏，如有发现，一律封号。如设备阅读收费，未付费状态下将只能查看全篇内容的10%" name="content"></textarea>        
-            <van-uploader :after-read="afterRead" v-model="fileList">
+            <van-uploader :after-read="afterRead" :before-read="beforeRead" v-model="fileList">
                 <img src="../img/photo.png" alt="" class="photo-submit">
             </van-uploader> 
         </div>
@@ -48,7 +48,17 @@ export default {
         afterRead(file) {
             // 此时可以自行将文件上传至服务器
             console.log(file);
-        }
+        },
+        beforeRead(file) {
+            if (file.type !== 'image/jpeg') {
+                if(file.type !== 'image/png'){
+                    if(file.type !== 'image/gif'){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        },
     }
 }
 </script>
