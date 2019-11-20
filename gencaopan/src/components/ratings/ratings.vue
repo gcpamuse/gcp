01c60027f5b41ab1cefd0c6f6f517cae905079dc
yangfan <872404816@qq.com>
2019-11-20 17:22:18 +0800

    1

--- gencaopan/src/components/ratings/ratings.vue
+++ gencaopan/src/components/ratings/ratings.vue
@@ -5,7 +5,7 @@
 
 			</div> 
 			<div class="swiper-wrapper">   
-				<div class="swiper-slide"> 
+				<!-- <div class="swiper-slide"> 
 					<a href="javascript:;" class="swiper-pos" onClick=""> 
 						<img src="../../img/zimeiti_lunbo1.jpg"  > 
 						<div class="swiper-ps">证券类投顾牛散将陆续入驻</div>
@@ -16,19 +16,19 @@
 						<img src="../../img/zimeiti_lunbo2.jpg"  > 
 						<div class="swiper-ps">【ONE-77】10个月累计收益2009%！交易五年，盈利五</div>
 					</a> 
-				</div>
-				<!-- <div class="swiper-slide"> 
+				</div> -->
+				<div class="swiper-slide" v-for="(image, index) in images" :index="index" :key="image.id"> 
 					<a href="javascript:;" class="swiper-pos" onClick=""> 
-						<img src="../../img/zimeiti_lunbo3.jpg"  > 
-						<div class="swiper-ps">为什么跟单不能挣钱！</div>
+						<img :src="image.img" /> 
+						<div class="swiper-ps">{{image.text}}</div>
 					</a> 
-				</div>  -->
-				<div class="swiper-slide"> 
+				</div> 
+				<!-- <div class="swiper-slide"> 
 					<a href="javascript:;" class="swiper-pos" onClick=""> 
 						<img src="../../img/zimeiti_lunbo3.jpg"  > 
 						<div class="swiper-ps">从3万到3600万，逻辑+价值投资的实践者</div>
 					</a> 
-				</div>  
+				</div>   -->
 			</div>
 		</div> 
 		<div class="zimeiti">自媒体</div>
@@ -67,6 +67,20 @@ import '../../../dist/static/css/swiper.min.css';
 		name:'ratings',
 		data(){
 			return{
+				images:[
+					{
+						img:'./static/img/zimeiti_lunbo1.68871e1.jpg',
+						text:'证券类投顾牛散将陆续入驻'
+					},
+					{
+						img:'./static/img/zimeiti_lunbo2.c82d195.jpg',
+						text:'【ONE-77】10个月累计收益2009%！交易五年，盈利五'
+					},
+					{
+						img:'./static/img/zimeiti_lunbo3.9d51a18.jpg',
+						text:'从3万到3600万，逻辑+价值投资的实践者'
+					}
+				],
 				list:[
 					{
 						img:'http://daoshi.simutz.com/attachments/mediaImages/1572014343221.png',
