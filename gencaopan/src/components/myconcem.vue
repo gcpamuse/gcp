e160dda9fa05c5ffd3126fe97a5271a250a31e72
yangfan <872404816@qq.com>
2019-11-23 16:24:05 +0800

    1

--- gencaopan/src/components/myconcem.vue
+++ gencaopan/src/components/myconcem.vue
@@ -89,7 +89,8 @@ export default {
                 that.loading = false;
 				let data = res.data.data.data;
                 // this.list = data.data;
-                if(data.data.length){
+                // if(data.data.length){
+                if(data.data){
                     this.loading = false;
                     // this.list = this.list.concat(data.data);
                     this.list.push(...data.data);
