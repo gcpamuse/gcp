8f2a76517b79c1a124ba34501a24a5e0940305af
yangfan <872404816@qq.com>
2019-11-26 11:33:58 +0800

    1

--- gencaopan/src/components/findPasswd.vue
+++ gencaopan/src/components/findPasswd.vue
@@ -6,7 +6,7 @@
                 v-model="phone"
                 placeholder="手机号码"
             >
-                <van-button slot="button" size="small" type="primary" style="background-color: #f24848;border: 1px solid #f24848;">获取短信验证码</van-button>
+                <van-button @click="countDown" slot="button" size="small" type="primary" style="background-color: #f24848;border: 1px solid #f24848;">{{content}}</van-button>
             </van-field>
             <van-field
                 v-model="number"
@@ -34,10 +34,47 @@ export default {
         return{
             phone:'',
             number:'',
-            password:''
+            password:'',
+            content: '获取短信验证码',
+            totalTime: 60,
+            canClick: true
         }
     },
     methods:{
+        countDown () {
+            if(this.phone == ''){
+                this.$toast('手机号不能为空！');
+                return false;
+            }else{
+                if(!(/^1[34578]\d{9}$/.test(this.phone))){
+                    this.$toast('请输入正确的手机号格式');
+                    return false;
+                }
+            }
+            if (!this.canClick) return  //改动的是这两行代码
+            this.canClick = false
+            this.content = this.totalTime + 'S后重新发送'
+            let clock = window.setInterval(() => {
+                this.totalTime--
+                this.content = this.totalTime + 'S后重新发送'
+                if (this.totalTime < 0) {
+                    window.clearInterval(clock)
+                    this.content = '重新发送验证码'
+                    this.totalTime = 10
+                    this.canClick = true
+                }
+            },1000)
+            var params = {
+                phone:this.phone, 
+                captcha:this.number
+            };
+            this.$axios.post('http://192.168.0.99:8080/',params).then( res=>{
+                console.log(res)
+            })
+            .catch( error=>{
+        　　　　console.log("出错喽："+error);
+        　　});
+        },
         scanCode(){
             if(this.phone == ''){
                 this.$toast('手机号不能为空！');
