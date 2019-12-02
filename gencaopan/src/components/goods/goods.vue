41b4016c6c94e8a3a423355223697b19f0f60184
nai <eamil@qq.com>
2019-12-02 12:15:02 +0800

    dd

--- gencaopan/src/components/goods/goods.vue
+++ gencaopan/src/components/goods/goods.vue
@@ -141,11 +141,11 @@ import { Button } from 'vant';
 		},
 		mounted(){
 			
-			this.$http.get('/api/teacherList').then((res) => {
+			// this.$http.get('/api/teacherList').then((res) => {
 			   
-				let data = res.data.data.data;
-   				this.list = data.data;
-			});
+			// 	let data = res.data.data.data;
+   			// 	this.list = data.data;
+			// });
 		}
 	}
 </script>
